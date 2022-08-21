a54cf25a36c03a396dc54bc9f1b9b3b2bebe202f
braije <idworkx@gmail.com>
2022-08-21 20:15:06 +0200

    Setup default middleware

--- api/core/middleware.js
+++ api/core/middleware.js
@@ -2,6 +2,9 @@
  * MIDDLEWARE
  */
 
+const helmet = require("helmet");
+const compression = require("compression");
+
 module.exports = $ => {
 
   /**
@@ -88,6 +91,22 @@ module.exports = $ => {
     }
 
   };
+
+  /**
+   * SETUP DEFAULT
+   */
+
+  let instance = $.server.instance();
+  let engine = $.server.engine();
+
+  instance.set('trust proxy', 1);
+  instance.set('etag', false);
+  instance.disable('x-powered-by');
+
+  $.middleware.add("helmet", 100, helmet());
+  $.middleware.add("compression", 120, compression());
+  $.middleware.add("json_limit", 140, engine.json({ limit: '10MB', extended: true }));
+  $.middleware.add("urlencoded_limit", 150, engine.urlencoded({ limit: '10MB', extended: true }));
 
   /**
    * COMMANDS
