cecedf87af78e8acb47f59b3c972f5979bff4158
braije <idworkx@gmail.com>
2022-09-14 00:04:50 +0200

    Draft request

--- api/core/request.js
+++ api/core/request.js
@@ -33,13 +33,84 @@ module.exports = $ => {
     });
     */
 
-    const get = () => {
+    const get = (url, options = {}) => {
 
+        // Default.
+        let params = { ... {
+            timeout: 15000
+        }, options}; 
+
+        // Wrap inside a promise to manage root error.
         return new Promise((resolve, reject) => {
+
+            // For the lonnng pooling request.
+            let timer;
+
+            try {
+                return fetch(url, {}).then(async res => {
+
+                    console.log(res.ok, res.status, params.timeout)
+
+                    // Fallback for long pooling.
+                    timer = setTimeout(() => {
+                        reject({code: 602, response: "Time out"});
+                    }, params.timeout);
+
+                    // 
+                    if (res.ok && res.status === 200) {
+
+                        clearTimeout(timer);
+
+                        let txt = await res.text();
+
+                        let json = false;
+
+                        try {
+                            json = JSON.parse(txt);
+                        }
+                        catch(e) {
+                            json = txt;
+                        }
+
+                        resolve({code: 200, response: json});
+
+                    }
+                    else {
+                        reject({code: 601, response: "Response not ok"});
+                    }
+                    
+
+                }).catch(e => {
+
+                    reject({code: 600, response: e.message });
+
+                })
+            }
+            catch(e) {
+                reject(e.message);
+            }
 
         });
 
     };
+
+    $.on("betiny:test", async () => {
+
+        await get("http://127.0.0.1:3033")
+            .then(res => console.log(res))
+            .catch(e => console.log(e));
+
+        await get("/")
+            .then(res => console.log(res))
+            .catch(e => console.log(e));
+
+        /*await get("https://jsonplaceholder.typicode.com/users")
+            .then(res => console.log(res))
+            .catch(e => console.log(e));*/
+
+            
+
+    });
 
     $.browser = {
         get: () => {},
